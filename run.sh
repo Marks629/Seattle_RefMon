9bdb07e0ecb66e9091a518bb79b8149b4f917253
Harshit Sanghvi <sanghvi.harshit@gmail.com>
2015-01-15 14:19:31 -0500

    updated README, Fixed run.sh and test case, added .travis.yml

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp a2p1_security_layer.r2py repy/
-cp a2p1_program.r2py repy/
+cp a2p1_test_program.r2py repy/
 cd repy/
-python repy.py restrictions.default encasementlib.r2py a2p1_security_layer.r2py a2p1_program.r2py
+python repy.py restrictions.default encasementlib.r2py a2p1_security_layer.r2py a2p1_test_program.r2py
 cd ..
